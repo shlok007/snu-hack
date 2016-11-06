db7eb22c14ddb526a7afee5aac1f4f4e37ae413e
shlok007 <shlok.srivastava15@gmail.com>
2016-11-06 05:09:44 -0500

    last 3 hours

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,22 +1,30 @@
 class StudentController < ApplicationController
 	def show
-		@student = Student.find(param[:id].to_i)
+		@student = Student.find(params[:id])
 	end
 
 	def attach_addhar
-		@addhar = Aadhar.where(number: params[:id])
-		@student = Student.find(params[:id].to_i)
-		if @addhar.name == @student.name
-			@exam_id = generate_id
-		else
+		@addhar = AddharCard.where(number: params[:number]).first
+		@student = Student.find_by_id(params[:id])
+		begin 
+			if !@exam_id = ExamId.where(aadhar_number: @addhar.number, student_id: @student.id).last.id + 1600
+			  if @student.name == @addhar.name && @student.fathers_name == @addhar.fathers_name
+			    @exam_id = generate_id
+			    render json: {:exam_id => @exam_id}
+			  end
+			else
+				render json: {:exam_id => @exam_id}
+			end
+		rescue
 			flash[:error] = "One of your fields did not match with the aadhar card you entered."
 			redirect_to :back
+		end
 	end
 
 	private
 
 	def generate_id
-		ExamId.create(addhar_number: @addhar.number, student_id: @student.id)
-		Exam.count - 1
+		ExamId.create(aadhar_number: @addhar.number, student_id: @student.id)
+		1600 + ExamId.count
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+
+	root 'navigation#index'
+	post 'student/attach_addhar', to: 'student#attach_addhar', as: 'attach_addhar'
+	get 'student/:id', to: 'student#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
